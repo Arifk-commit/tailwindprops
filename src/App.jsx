12be3bca96ebf3842d0698e85e6d713d7a3dfb33
Arifk-commit <khanarifshamshul@gmail.com>
2025-01-17 22:21:20 +0530

    another day of commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let newobj = {
+    username: "Arif" ,
+    Age: 21
+  }
+  let newobj2 = {
+    username2: "Arif's Desk" ,
+    Age2: 21
+  }
+  let newobj3 = {
+    username3: "Saqif's Desk" ,
+    Age3: 21
+  }
+  let newarr = [1,2,3]
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1 className='bg-green-400 text-white rounded-xl p-4'> Hello Tailwind</h1>
+     <Card username = "Arif"  someobj = {newobj} someobj2 = {newobj2.username2}/>
+     <Card someobj2={newobj3.username3} />
+
     </>
   )
 }
